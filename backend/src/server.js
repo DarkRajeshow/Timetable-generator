909d4037c550b46fe52dc1a958b684127e8d1c19
DarkRajeshow <rajannaadeli@gmail.com>
2024-12-26 15:21:39 +0530

    Fixed vercel.json file

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,6 @@ import app from './app.js';
 
 const PORT = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-  res.send("Backend is Working.")
-})
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
